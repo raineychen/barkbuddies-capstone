a5c75287667bfce166af03aa7c6f8bf1abc75bf0
Rainey <chen.rainey@gmail.com>
2023-09-01 18:38:46 -0700

    making swiper component for pet swipes

--- bb-client/src/components/Swipe/Swipe.js
+++ bb-client/src/components/Swipe/Swipe.js
@@ -1,44 +1,81 @@
 import "./Swipe.scss";
-import Footer from "../Footer/Footer";
-import userProfile from "../../assets/Images/profile_user.png";
-import heart from "../../assets/Images/heart.png";
-import info from "../../assets/Images/info.png";
-import xMark from "../../assets/Images/x_mark.png";
-import { Link } from "react-router-dom";
+// import Footer from "../Footer/Footer";
+// import userProfile from "../../assets/Images/profile_user.png";
+// import heart from "../../assets/Images/heart.png";
+// import info from "../../assets/Images/info.png";
+// import xMark from "../../assets/Images/x_mark.png";
+// import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap'; 
+import 'react-id-swiper/lib/styles/css/swiper.css'; 
+import 'swiper/swiper-bundle.css'; 
+import { Swiper, SwiperSlide } from 'swiper/react'; 
+
+
+const dummyData = [
+  {
+    id: 5,
+    dogName: "Buddy",
+    ownerName: "John Smith",
+    dogAge: 3,
+    dogBreed: "Golden Retriever",
+    dogGender: "Male",
+    photoURL: "https://example.com/buddy.jpg",
+  },
+  {
+    id: 4,
+    dogName: "Luna",
+    ownerName: "Emily Johnson",
+    dogAge: 2,
+    dogBreed: "Labrador Retriever",
+    dogGender: "Female",
+    photoURL: "https://example.com/luna.jpg",
+  },
+  {
+    id: 2,
+    dogName: "Max",
+    ownerName: "Michael Davis",
+    dogAge: 4,
+    dogBreed: "German Shepherd",
+    dogGender: "Male",
+    photoURL: "https://example.com/max.jpg",
+  },
+  {
+    id: 3,
+    dogName: "Lucy",
+    ownerName: "Sophia Wilson",
+    dogAge: 1,
+    dogBreed: "Poodle",
+    dogGender: "Female",
+    photoURL: "https://example.com/lucy.jpg",
+  },
+  {
+    id: 1,
+    dogName: "Charlie",
+    ownerName: "Olivia Martinez",
+    dogAge: 5,
+    dogBreed: "Bulldog",
+    dogGender: "Male",
+    photoURL: "https://example.com/charlie.jpg",
+  },
+];
+
 
 function Swipe() {
   return (
-    <div className="swipe">
-      <div className="swipe__hero">
-        <div className="swipe__info">
-          <h3 className="swipe__info--name">Atticus</h3>{" "}
-          <p className="swipe__info--age">1 year</p>
-        </div>
-        <div className="swipe__detail">
-          <p className="swipe__info--gender">Male</p>{" "}
-          <p className="swipe__info--breed">Pomerian</p>{" "}
-          <p className="swipe__info--distance">0.5km away</p>
-        </div>
-      </div>
-      <div className="swipe__buttons">
-        <div className="swipe__buttons--user">
-          <img src={userProfile} alt="user profile" className="swipe__icon" />
-          <p className="swipe__owner"> Tina</p>
-        </div>
-        <div className="swipe__buttons--interation">
-          <Link to={'/swipe/reject'}>
-          <img className="swipe__icons" src={xMark} alt="x-mark" />
-          </Link>
-          <Link to={'/swipe/info'}>
-          <img className="swipe__icon" src={info} alt="info" />
-          </Link>
-          <Link to={'/swipe/message'}>
-          <img className="swipe__icons" src={heart} alt="heart icon" />
-          </Link>
-        </div>
-      </div>
-      <Footer />
-    </div>
+    <div className="container">
+    <Swiper
+      slidesPerView={1}
+      onSwiper={(swiper) => {}}
+      onSlideChange={() => {}}
+    >
+      {dummyData.map((card, index) => (
+        <SwiperSlide key={index} className="card">
+          <div className="text">{card}</div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+    <Button onClick={() => console.log('oulala')}>You can press me</Button>
+  </div>
   );
 }
 
